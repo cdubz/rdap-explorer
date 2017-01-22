4057981896b962cd0ec5be06a604346690f615ab
Chris Wells <chris@chrxs.net>
2017-01-21 22:05:34 -0500

    Add project info and stamp version 0.0.1.

---
+++ rdap_explorer/__init__.py
@@ -0,0 +1,5 @@
+__title__ = 'RDAP Explorer'
+__version__ = '0.0.1'
+__author__ = 'Christopher Charbonneau Wells'
+__copyright__ = 'Copyright (c) 2017 Christopher Charbonneau Wells'
+__license__ = 'MIT'
