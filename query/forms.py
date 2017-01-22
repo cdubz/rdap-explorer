a9176b1fc9116601a98c53a84cff57d9692e1fa4
Chris Wells <chris@chrxs.net>
2017-01-21 21:25:21 -0500

    Remove label and add placeholder to Query field.

--- query/forms.py
+++ query/forms.py
@@ -6,4 +6,8 @@ from django import forms
 
 
 class QueryForm(forms.Form):
-    query = forms.CharField(max_length=100)
+    query = forms.CharField(
+        label='',
+        max_length=100,
+        widget=forms.TextInput(attrs={'placeholder': 'IPv4/6 address'})
+    )
