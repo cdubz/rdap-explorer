06bed92f55396c33a4f55c109fdb7871737fea7a
Chris Wells <chris@chrxs.net>
2017-01-25 05:58:58 -0500

    Add HTML5 autofocus attribute to query form field.

--- query/forms.py
+++ query/forms.py
@@ -10,6 +10,7 @@ class QueryForm(forms.Form):
         label='',
         max_length=45,  # Max length of an IPv6 address.
         widget=forms.TextInput(attrs={
+            'autofocus': 'autofocus',
             'class': 'form-input input-lg',  # TODO: Move this in theme!
             'placeholder': 'IPv4/6 address'
         })
