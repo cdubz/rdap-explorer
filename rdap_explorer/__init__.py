841bd456b0892ccb88563960ca6033f258ce2b23
Chris Wells <chris@chrxs.net>
2017-02-05 14:32:08 -0500

    Update APNIC website RDAP info link.

--- rdap_explorer/__init__.py
+++ rdap_explorer/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'RDAP Explorer'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Christopher Charbonneau Wells'
 __copyright__ = 'Copyright (c) 2017 Christopher Charbonneau Wells'
 __license__ = 'MIT'
